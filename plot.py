eb6c69227b68e4d979194d7c725aa198712a269f
5_vania_5 <antonivan1234567@gmail.com>
2025-07-29 23:46:19 +0300

    added candle graph for random walk

--- plot.py
+++ plot.py
@@ -1,25 +1,10 @@
 import plotly.graph_objects as go
 import plotly.io as pio
 
-# Configure Kaleido for image generation
+# Configure Kaleido
 pio.kaleido.scope.default_format = 'png'
 pio.kaleido.scope.default_width = 800
 pio.kaleido.scope.default_height = 600
-
-
-def create_line_figure(df):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=df['time'], y=df['price'], mode='lines+markers', name='Price'
-    ))
-    fig.update_layout(
-        title='Price Chart',
-        xaxis=dict(rangeslider_visible=True),
-        yaxis=dict(title='Price ($)'),
-        showlegend=False
-    )
-    return fig
-
 
 def create_candlestick_figure(candles):
     df = candles.copy()
@@ -35,21 +20,17 @@ def create_candlestick_figure(candles):
             yaxis='y2', marker_color='grey'
         )
     ])
+    # Add wallet count annotations
     annotations = [
-        dict(
-            x=row['time'], y=row['high'] * 1.01,
-            text=str(int(row['wallet_count'])),
-            showarrow=False, font=dict(size=8)
-        ) for _, row in df.iterrows()
+        dict(x=row['time'], y=row['high'] * 1.01,
+             text=str(int(row['wallet_count'])), showarrow=False, font=dict(size=8))
+        for _, row in df.iterrows()
     ]
     fig.update_layout(
         title='Candlestick Chart',
         xaxis=dict(rangeslider_visible=False),
         yaxis=dict(title='Price ($)'),
-        yaxis2=dict(
-            title='Volume ($)', overlaying='y', side='right',
-            showgrid=False, position=0.15
-        ),
+        yaxis2=dict(title='Volume', overlaying='y', side='right', showgrid=False, position=0.15),
         annotations=annotations,
         legend=dict(orientation='h', y=1.02)
     )
